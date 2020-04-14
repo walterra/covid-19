1c6887615b75fd2b2b3a117721461ce9e38196bc
Walter Rafelsberger <walter@elastic.co>
2020-04-14 08:59:44 +0200

    Initial website.

---
+++ src-www/vega_renderer.js
@@ -0,0 +1,18 @@
+
+var supportedKeys = ['chart_1', 'chart_2'];
+function loadVega(f, id) {
+  if (!supportedKeys.includes(f)) {
+    console.error('unsupported key:', f);
+    return;
+  }
+
+  $.get('data/'+f+'.json', function( spec ) {
+    var opt = {"renderer": "canvas", "actions": false};
+    vegaEmbed("#"+id, spec, opt);
+  });
+}
+
+$(document).ready(function(){
+  loadVega('chart_1', 'vega_chart_1');
+  loadVega('chart_2', 'vega_chart_2');
+});
