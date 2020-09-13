1fee80ee37f8f465de09502f83bc5d7d59f007d0
Walter Rafelsberger <walter@elastic.co>
2020-09-13 16:12:55 +0200

    Added Daniel Breuss as data source.

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -1,2 +1,3 @@
 curl https://corin.at/raw.php?format=csv > ./data/corin_data.csv
+curl https://raw.githubusercontent.com/Daniel-Breuss/covid-data-austria/master/austriadata.csv > ./data/breuss_data.csv
 curl https://covid.ourworldindata.org/data/owid-covid-data.csv > ./data/owid-covid-data.csv
