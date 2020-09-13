6342f240edca7f502f8000405c032770d9b27dfc
Walter Rafelsberger <walter@elastic.co>
2020-09-13 13:42:35 +0200

    Charts about tests.

--- src/vega_renderer.js
+++ src/vega_renderer.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 var supportedKeys = [
   'chart_1',
   'chart_2',
+  'chart_2test',
   'chart_3',
   'chart_4',
   'chart_counties_confirmed',
@@ -34,6 +35,7 @@ $(document).ready(function(){
   loadMetaData();
   loadVega('chart_1', 'vega_chart_1');
   loadVega('chart_2', 'vega_chart_2');
+  loadVega('chart_2test', 'vega_chart_2test');
   loadVega('chart_3', 'vega_chart_3');
   loadVega('chart_4', 'vega_chart_4');
   loadVega('chart_counties_confirmed', 'vega_chart_counties_confirmed');
